5dcfde806941aa4f9164202fc66b1fbd0093d56d
bazera <bazera.g@gmail.com>
2023-03-15 12:07:50 +0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+
+interface Message {
+  text: string;
+  name: string;
+  self: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'socket-lecture-front';
+export class AppComponent implements OnInit {
+  socket: Socket | undefined;
+  model: string = '';
+  user: string = 'bazera';
+
+  messages: Message[] = [];
+
+  sendMessage() {
+    if (!this.model) {
+      return;
+    }
+
+    const message = {
+      text: this.model,
+      name: this.user,
+      self: true,
+    };
+
+    this.socket?.emit('send-message', message);
+    this.messages.push(message);
+
+    this.model = '';
+  }
+
+  handleKey(event: any) {
+    if (event.keyCode === 13) {
+      this.sendMessage();
+    }
+  }
+
+  ngOnInit() {
+    this.socket = io('http://localhost:3000');
+
+    this.socket.on('receive-message', (message) => {
+      this.messages.push({
+        text: message.text,
+        name: message.name,
+        self: false,
+      });
+    });
+  }
 }
